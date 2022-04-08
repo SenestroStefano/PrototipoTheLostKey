b5ea3054cd4cf6c06386e5c4c5bebb540ee85b0e
SenestroStefano <senestrostefano003@gmail.com>
2022-04-08 13:32:01 +0200

    test

--- mostro.py
+++ mostro.py
@@ -1,4 +1,3 @@
-from numpy import angle
 import pygame, sys, math
 import global_var as GLOB
 
@@ -17,9 +16,6 @@ class Mostro(pygame.sprite.Sprite):
         self.angle = 90
         self.angle_triangle = 0
 
-        self.larg = 50
-        self.lung = 0
-
     def aggiorna(self):
         radius = 360
         rot_vector = self.line_vector.rotate(self.angle) * radius
@@ -30,36 +26,17 @@ class Mostro(pygame.sprite.Sprite):
         end_line = round(self.x + self.width/2 - rot_vector.x), round(self.y - rot_vector.y)
         self.line = pygame.draw.line(GLOB.screen, (5,80,255), start_line, end_line, 8)
 
+        larg = 10
+        lunghezza1 = self.x +self.width/2 + larg
+        lunghezza2 = self.x +self.width/2 - larg
+
 
         if self.angle == 360 or self.angle == -360:
             self.angle = 0
-
-
-        if self.angle == 90 or self.angle == -90 or self.angle == 270 or self.angle == -270:
-            self.larg = 50
-            self.lung = 0
-        else:
-            self.larg -= 1
-            self.lung = 50
-
-        if self.angle == 0 or self.angle == 360 or self.angle == -180 or self.angle == 180:
-            self.lung = 50
-            self.larg = 0
-        else:
-            self.lung += 1
-            self.larg = 50
-
-        lunghezza1 = (self.x - self.larg) -rot_vector.x
-        lunghezza2 = (self.x + self.larg) -rot_vector.x
-
-        larghezza1 = (self.y - self.lung) -rot_vector.y
-        larghezza2 = (self.y + self.lung) -rot_vector.y
-
+            
         fine = round(self.y - rot_vector.y)
 
-        print(" Lungh: ", self.lung,  " Largh: ", self.larg, "Angle: ", self.angle)
-
-        self.triangle = pygame.draw.polygon(surface=GLOB.screen, color=(255, 0, 0),points=[start_line, (lunghezza1, larghezza1), (lunghezza2, larghezza2)])
+        self.triangle = pygame.draw.polygon(surface=GLOB.screen, color=(255, 0, 0),points=[start_line, (lunghezza1, fine), (lunghezza2, fine)])
 
         val = 2
 
@@ -74,6 +51,8 @@ class Mostro(pygame.sprite.Sprite):
         # mask = pygame.mask.from_surface(mask_image)
 
         GLOB.screen.blit(immagine, (GLOB.screen_width/2 - int(immagine.get_width()/2)  - rot_vector.x/2, GLOB.screen_height/2 - int(immagine.get_height()/2) - rot_vector.y/2))
+
+        print(" Punto X: ",start,  " Punto Y: ", end)
 
         # rect = pygame.Rect(MENU_MOUSE_POS[0], MENU_MOUSE_POS[1], 1, 1) 
         # print(rect)
